3055f72f0676636acf1ee66b89ef22a231a7bdcc
RinkiyaKeDad <arshsharma461@gmail.com>
2020-06-26 18:26:21 +0530

    final changes done

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ const App = () => {
         const response = await axios.get(
           `https://sleepy-garden-81515.herokuapp.com/headlines/`
         );
-        console.log(response.data.headlines);
+        //console.log(response.data.headlines);
         setLoadedHeadlines(response.data.headlines);
       } catch (err) {
         console.log(err);
@@ -35,22 +35,48 @@ const App = () => {
   }, [loadedHeadlines, curPage]);
 
   return (
-    <div className='container'>
-      <h1>Headlines</h1>
-      <ul class='list-group'>
+    <div className='container main-wrapper'>
+      <h1 style={{ margin: '1rem 1rem .1rem 1rem' }}>Headlines</h1>
+      <p>
+        Web Scrapping using Node.js and Express. Pagation using Hooks in React.
+      </p>
+      <ul className='list-group' style={{ margin: '.4rem 1rem 1rem 1rem' }}>
         {displayedHeadlines &&
-          displayedHeadlines.map(headline => (
-            <li class='list-group-item'>{headline}</li>
+          displayedHeadlines.map((headline, index) => (
+            <li key={index} className='list-group-item'>
+              {headline}
+            </li>
           ))}
       </ul>
-      <ul class='pagination'>
-        {array.map(num => (
-          <li class='page-item'>
-            <a class='page-link' href='#' onClick={() => setCurPage(num)}>
-              {num}
-            </a>
-          </li>
-        ))}
+      <ul
+        className='pagination flex-list'
+        style={{ margin: '.4rem 1rem 1rem 1rem' }}
+      >
+        {array.map(num => {
+          if (num === curPage)
+            return (
+              <li
+                key={num}
+                className='page-item active'
+                style={{ margin: '.1rem' }}
+              >
+                <a
+                  className='page-link'
+                  href='#'
+                  onClick={() => setCurPage(num)}
+                >
+                  {num}
+                </a>
+              </li>
+            );
+          return (
+            <li key={num} className='page-item' style={{ margin: '.1rem' }}>
+              <a className='page-link' href='#' onClick={() => setCurPage(num)}>
+                {num}
+              </a>
+            </li>
+          );
+        })}
       </ul>
     </div>
   );
